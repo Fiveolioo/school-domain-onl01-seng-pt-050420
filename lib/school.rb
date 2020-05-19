e0ee95a6d99a890b1afde13423c031913ec02304
Richard Cho <96richardohc@gmail.com>
2020-05-19 01:36:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,12 +8,12 @@ class School
   end
   
   def add_student(name, grade)
-  if @roster.has_key?(grade)
-    @roster[grade] << name
-  else 
-    @roster[grade] = []
-    @roster[grade] << name
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else 
+      @roster[grade] = []
+      @roster[grade] << name
+    end
   end
-end
     
 end
